318137de204384d1299f6c2df455c5b402eb76e8
Abhigyan Saikia <karma.dinesty@gmail.com>
2026-09-24 12:32:48 +0530

    fix: remove character completely from hero on all views

--- public/assets-unifex/js/custom-gsap.js
+++ public/assets-unifex/js/custom-gsap.js
@@ -625,21 +625,6 @@
     );
   });
 
-  ///////////////////////
-  // 11.5 Hero Character Parallax
-  let heroParallaxMM = gsap.matchMedia();
-  heroParallaxMM.add("(max-width: 1199px)", () => {
-    gsap.to(".hero-character-wrapper img", {
-      y: 100, // Moves down slightly as user scrolls down
-      scrollTrigger: {
-        trigger: ".hero-transplant",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-        markers: false,
-      }
-    });
-  });
 
   ///////////////////////
   // 12. Hero Title Rotation
